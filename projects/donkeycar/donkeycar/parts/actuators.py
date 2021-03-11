3acf070934303059907888ce9da4f926270af853
ejw0607 <78245120+ejw0607@users.noreply.github.com>
2021-03-11 17:20:05 -0600

    Create actuators.py

---
+++ projects/donkeycar/donkeycar/parts/actuators.py
@@ -0,0 +1,56 @@
+class PWMThrottle:
+
+# Wrapper over a PWM motor cotnroller to convert -1 to 1 throttle
+# values to PWM pulses.
+
+MIN_THROTTLE = -1
+MAX_THROTTLE = 1
+
+def __init__(self, controller=None,
+                   max_pulse=4095,
+                   min_pulse=-4095,
+                   zero_pulse=0):
+
+    self.controller = controller
+    self.max_pulse = max_pulse
+    self.min_pulse = min_pulse
+    self.zero_pulse = zero_pulse
+    
+    #send zero pulse to calibrate ESC
+    print("Init ESC")
+    self.controller.set_pulse(self.zero_pulse)
+    time.sleep(1)
+
+
+def run(self, throttle):
+    if throttle > 0:
+        pulse = dk.utils.map_range(throttle,
+                                0, self.MAX_THROTTLE, 
+                                self.zero_pulse, self.max_pulse)
+        self.controller.pwm.set_pwm(self.controller.channel,0,pulse)
+        self.controller.pwm.set_pwm(self.controller.channel+1,0,0)
+        self.controller.pwm.set_pwm(self.controller.channel+2,0,4095)
+        self.controller.pwm.set_pwm(self.controller.channel+3,0,0)
+        self.controller.pwm.set_pwm(self.controller.channel+4,0,pulse)
+        self.controller.pwm.set_pwm(self.controller.channel+7,0,pulse)
+        self.controller.pwm.set_pwm(self.controller.channel+6,0,0)
+        self.controller.pwm.set_pwm(self.controller.channel+5,0,4095)
+    else:
+        pulse = dk.utils.map_range(throttle,
+                                self.MIN_THROTTLE, 0, 
+                                self.min_pulse, self.zero_pulse)
+        #self.controller.pwm.set_pwm(self.controller.channel,0,self.zero_pulse)**
+        #self.controller.pwm.set_pwm(self.controller.channel,0,pulse)**
+        #self.controller.pwm.set_pwm(self.controller.channel,0,self.zero_pulse)**
+        #self.controller.pwm.set_pwm(self.controller.channel,0,pulse)**
+         self.controller.pwm.set_pwm(self.controller.channel,0,- pulse)
+         self.controller.pwm.set_pwm(self.controller.channel+2,0,0)
+         self.controller.pwm.set_pwm(self.controller.channel+1,0,4095)
+         self.controller.pwm.set_pwm(self.controller.channel+3,0,- pulse)
+         self.controller.pwm.set_pwm(self.controller.channel+4,0,0)
+         self.controller.pwm.set_pwm(self.controller.channel+7,0,- pulse)
+         self.controller.pwm.set_pwm(self.controller.channel+5,0,0)
+         self.controller.pwm.set_pwm(self.controller.channel+6,0,4095)
+
+def shutdown(self):
+    self.run(0) #stop vehicle
